3d149a50883a81111c45e7174a178771fc67d46a
Wolfgang Fahl <wf@bitplan.com>
2026-03-28 14:41:28 +0100

    fixes version

--- mwstools_backend/tsite.py
+++ mwstools_backend/tsite.py
@@ -19,7 +19,7 @@ from typing import Dict, Iterable, Iterator, List, TypeVar
 from basemkit.base_cmd import BaseCmd
 from basemkit.persistent_log import Log
 from basemkit.profiler import Profiler
-from frontend.version import Version
+from mwstools_backend.version import Version
 from tqdm import tqdm
 from wikibot3rd.smw import SMWClient
 from wikibot3rd.wikiclient import WikiClient
